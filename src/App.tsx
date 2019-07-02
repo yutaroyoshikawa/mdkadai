44981d3e52b2c88fb2ebfb29efdb11d5988f514f
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2019-07-02 20:02:23 +0900

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import * as React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Index from './layouts'
+import About from './layouts/about'
+import WorksDetails from './layouts/worksDetails'
 
-class App extends React.Component {
-  public render() {
-    return (
-      <Router>
-        <Switch>
-          <Route path="/" component={Index} exact={true} /> 
-        </Switch>
-      </Router>
-    )
-  }
+const App: React.SFC = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Index} exact={true} />
+        <Route path="/about" component={About} exact={true} />
+        <Route path="/worksDetails" component={WorksDetails} exact={true} />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
