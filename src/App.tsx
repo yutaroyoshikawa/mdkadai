2705ec0c702f46414e4256f6f3099333ab91f606
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2019-07-06 23:36:16 +0900

    [ADD]ルーティング追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,25 @@ import * as React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Index from './layouts'
 import About from './layouts/about'
-import WorksDetails from './layouts/worksDetails'
+import Menu from './layouts/menu'
+import Opening from './layouts/opening'
+import Works from './layouts/works'
+import { Global, LogoWrap, MenuWrap } from './styles/app'
 
 const App: React.SFC = () => {
   return (
     <Router>
+      <LogoWrap>
+        <Opening />
+      </LogoWrap>
+      <Global />
+      <MenuWrap>
+        <Menu />
+      </MenuWrap>
       <Switch>
         <Route path="/" component={Index} exact={true} />
         <Route path="/about" component={About} exact={true} />
-        <Route path="/worksDetails" component={WorksDetails} exact={true} />
+        <Route path="/works/:name" component={Works} exact={true} />
       </Switch>
     </Router>
   )
